2177fd1dd9e029e166a8b4483333be186822d280
MicroTot <32261713+MicroTot@users.noreply.github.com>
2021-11-22 12:56:44 +0300

    MongoDB

--- lights/settings.py
+++ lights/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'lights.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'kibet',
     }
 }
-
+# ‘default’: {
+#       ‘ENGINE’: ‘djongo’,
+#       ‘NAME’: ‘your-db-name’,
+#    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
